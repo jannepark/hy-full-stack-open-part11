c7ca0b47c7f82bdb18e7119c24ff270fc6778c07
n8761 <n8761@student.jamk.fi>
2024-04-24 15:17:40 +0000

    testing secrets github actions

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-// const MONGODB_URI = process.env.NODE_ENV === 'test'
-//     ? process.env.TEST_MONGODB_URI
-//     : process.env.MONGODB_URI
-
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
-    ? 'mongodb+srv://janneviljo:Promoter-Hulk0-Sandal@cluster0.5irxesk.mongodb.net/forTheTests?retryWrites=true&w=majority&appName=Cluster0'
+    ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI
+
+// const MONGODB_URI =
+//   process.env.NODE_ENV === 'test'
+//     ? 'mongodb+srv://janneviljo:Promoter-Hulk0-Sandal@cluster0.5irxesk.mongodb.net/forTheTests?retryWrites=true&w=majority&appName=Cluster0'
+//     : process.env.MONGODB_URI
 module.exports = {
   MONGODB_URI,
   PORT,
